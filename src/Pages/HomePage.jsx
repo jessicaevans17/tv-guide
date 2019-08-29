fb2f7fa5343db965bd26ccc8d200d85e0f3785d6
Nadia Murchison <nadiamurchison@gmail.com>
2019-08-29 17:55:57 -0400

    homepage wireframe started

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,8 +1,29 @@
-import React from "react"
-import Header from "../components/Header"
+import React from 'react'
+import Header from '../components/Header'
 
 const HomePage = () => {
-  return <></>
+  return (
+    <>
+      <main className="home-page-main">
+        <section>
+          <h1>Top-Rated TV Shows</h1>
+          <img
+            className="randomTVimage"
+            src="https://i.ytimg.com/vi/l4bDVq-nP-0/maxresdefault.jpg"
+          />
+          <h2>Title</h2>
+          <h2>Rating</h2>
+        </section>
+        <section>
+          <img src className="tv-item" />
+          <div className="tv-item-info">
+            <h3>Title</h3>
+            <h3>Rating</h3>
+          </div>
+        </section>
+      </main>
+    </>
+  )
 }
 
 export default HomePage
