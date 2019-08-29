3ec81012a8593eb507e5270db38153734a54a805
Jessica Evans <jme@mail.usf.edu>
2019-08-29 16:39:31 -0400

    added html

---
+++ src/Pages/CastPage.jsx
@@ -0,0 +1,41 @@
+import React from "react"
+
+const CastPage = () => {
+  return (
+    <>
+      <section className="cast-page">
+        <section className="show-information">
+          <img src="" alt="" />
+          <h1>Title of Show </h1>
+          <h2>TV Show Rating</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur
+            fuga illo alias corrupti repellat exercitationem numquam ipsam!
+            Voluptatem quaerat fugiat voluptate incidunt deserunt eligendi,
+            itaque veritatis illum inventore similique sint.
+          </p>
+        </section>
+        <section className="cast-and-crew-info">
+          <section className="cast-information">
+            <h2>Cast Information</h2>
+            <section className="cast-and-crew-card">
+              <img src="" alt="" />
+              <h3>Character Name:</h3>
+              <h3>Actor Name:</h3>
+            </section>
+          </section>
+          <section className="crew-information">
+            <h2>Crew Information</h2>
+            <section className="cast-and-crew-card">
+              <img src="" alt="" />
+              <h3>Name</h3>
+              <h3>Job Title</h3>
+            </section>
+          </section>
+        </section>
+      </section>
+    </>
+  )
+}
+
+export default CastPage
