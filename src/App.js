8280351fbbc518fe7a8979f94147190b02437655
Jessica Evans <jme@mail.usf.edu>
2019-08-29 16:59:02 -0400

    got routes working!!!

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
-import Header from './components/Header'
+import React, { Component } from "react"
+import HelloWorld from "./components/HelloWorld"
+import Header from "./components/Header"
+import CastPage from "./Pages/CastPage"
+import HomePage from "./Pages/HomePage"
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom"
 
 class App extends Component {
   render() {
     return (
       <>
-        <Header />
+        <Router>
+          <Header />
+          <Switch>
+            <Route exact path="/" component={HomePage}></Route>
+            <Route exact path="/cast-page" component={CastPage}></Route>
+          </Switch>
+        </Router>
       </>
     )
   }
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,12 +1,8 @@
-import React from 'react'
-import Header from '../components/Header'
+import React from "react"
+import Header from "../components/Header"
 
 const HomePage = () => {
-  return (
-    <>
-      <Header />
-    </>
-  )
+  return <></>
 }
 
 export default HomePage
