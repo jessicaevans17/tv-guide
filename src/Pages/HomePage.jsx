e347e446c51ba6de2eadcf52f8a94c636ba8cb48
Nadia Murchison <nadiamurchison@gmail.com>
2019-09-02 18:13:03 -0400

    random show works now, need next page to work

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -3,9 +3,9 @@ import axios from 'axios'
 import { Link } from 'react-router-dom'
 
 const HomePage = () => {
-  const [tvShow, setTvShow] = useState([])
+  const [tvShow, setTvShow] = useState([[]])
   const [currentPage, setCurrentPage] = useState(1)
-  const [random, setRandom] = useState([0])
+  const [random, setRandom] = useState(0)
 
   const fetchData = async () => {
     const resp = await axios.get(
@@ -14,9 +14,7 @@ const HomePage = () => {
     console.log(resp.data.results)
     setTvShow(resp.data.results)
     setCurrentPage(resp.data.page)
-    setRandom(
-      resp.data.results[Math.floor(Math.random) * resp.data.results.length - 1]
-    )
+    setRandom(Math.floor(Math.random() * resp.data.results.length))
   }
 
   const showNextPage = () => {
@@ -31,23 +29,15 @@ const HomePage = () => {
     <>
       <main className="home-page-main">
         <h1>Top-Rated TV Shows</h1>
-        Page: {currentPage}
         <div>
-          {/* {random.map((randomShow, i) => {
-            return (
-              <div key={i}>
-                <img
-                  src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2${randomShow.poster_path}`}
-                />
-                <h2>Title: {randomShow.name}</h2>
-                <h3>Rating: {randomShow.vote_average}</h3>
-              </div>
-            )
-          })} */}
+          <Link to={'/cast-page'}>
+            <img
+              src={`https://image.tmdb.org/t/p/w200${tvShow[random].poster_path}`}
+            />
+            <h2>Title: {tvShow[random].name}</h2>
+          </Link>
+          <h3>Rating: {tvShow[random].vote_average}</h3>
         </div>
-        <nav>
-          <button onClick={showNextPage}>Next</button>
-        </nav>
         <section className="tv-item-section">
           <ul className="tv-show">
             {tvShow.map((show, i) => {
@@ -64,6 +54,11 @@ const HomePage = () => {
               )
             })}
           </ul>
+          <p>Page: {currentPage}</p>
+
+          <nav>
+            <button onClick={showNextPage}>Next</button>
+          </nav>
         </section>
       </main>
     </>
